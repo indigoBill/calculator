b2c1e442c5d606e786532cbf068943796a0e71ba
indigoBill <117851497+indigoBill@users.noreply.github.com>
2023-09-16 20:47:56 -0400

    Fix bugs that occur when numbers are entered

--- script.js
+++ script.js
@@ -24,6 +24,9 @@ function operate(operator, operand1, operand2){
         case '*':
             return multiply(operand1,operand2);
         case '/':
+            if(operand2 === 0){
+                return 'NO DIV BY 0'; //ERROR OCCURS SINCE .toFixed REQUIRES A NUMBER
+            }
             return divide(operand1,operand2);
     }
 }
@@ -50,10 +53,27 @@ function displayDigits(e){
     }
 
     if(firstOperandUsed){
+        console.log('2nd');
+        
+        //IF NUM IS PRESSED AFTER OPERATION IS COMPLETE IT WILL
+        //CLEAR THE SCREEN AND START A NEW OPERATION
+        let topExpression = topDisplay.textContent;
+        let expressionArr = topExpression.split(' ');
+        let arrLength = expressionArr.length;
+
+        if(expressionArr[arrLength - 1] !== operator){
+            clearScreen();
+            firstOperand = e.target.textContent;
+            displayValue.push(firstOperand);
+            bottomDisplay.textContent = firstOperand;
+            return;
+        }
+        
         secondOperand = displayValue.join('');
         bottomDisplay.textContent = secondOperand;
         secondOperandUsed = true;
     } else{
+        console.log('1st');
         firstOperand = displayValue.join('');
         bottomDisplay.textContent = firstOperand;
     }
@@ -64,18 +84,19 @@ operatorButtons.forEach(operator => operator.addEventListener('click', addOperat
 
 function addOperator(e){
     if(firstOperandUsed && secondOperandUsed){
-        result = operate(operator, +firstOperand, +secondOperand);
+        checkForDecimals();
         topDisplay.textContent = result;
     }
 
     if(secondOperandUsed){
+        
         topDisplay.textContent = result.toString().concat(' ', e.target.textContent);
     } else {
         topDisplay.textContent = firstOperand.concat(' ', e.target.textContent);
     }
 
     displayValue.splice(0, displayValue.length);
-    bottomDisplay.textContent = 'EMPTY';
+    bottomDisplay.textContent = result;
     firstOperandUsed = true;
     operator = e.target.textContent;
 
@@ -85,24 +106,53 @@ const equalButton = document.querySelector('.equal-button');
 equalButton.addEventListener('click', equalsTo);
 
 function equalsTo(){
-
+    
     if(!firstOperandUsed || !secondOperandUsed){
         bottomDisplay.textContent = 'ERROR';
+        displayValue.splice(0, displayValue.length);
         return;
     }
 
     let currDisplayed = topDisplay.textContent;
 
     if(!(currDisplayed.split(' ').includes(secondOperand, 1))){
-        console.log('used');
-    
         topDisplay.textContent = currDisplayed.concat(' ', secondOperand);
     }
 
-    result = operate(operator, +firstOperand, +secondOperand);
+    checkForDecimals();
+    
     bottomDisplay.textContent = result;
    
 }
 
+function checkForDecimals(){
+
+    if(firstOperand.includes('.') || secondOperand.includes('.')){
+        result = (operate(operator, +firstOperand, +secondOperand)).toFixed(2);
+    }else{
+
+        result = operate(operator, +firstOperand, +secondOperand);
+        if(result.toString().includes('.')){
+            let valHold = result.toFixed(2);
+            result = +valHold;
+        }
+    }
+}
+
+const clearButton = document.querySelector('.erase-buttons');
+
+clearButton.addEventListener('click', clearScreen);
+
+function clearScreen(){
+    displayValue.splice(0, displayValue.length);
+    operator = '';
+    firstOperand = '';
+    secondOperand = '';
+    result = '';
+    firstOperandUsed = false;
+    secondOperandUsed = false;
+    bottomDisplay.textContent = 'BOTTOM';
+    topDisplay.textContent = 'TOP';
+}
 
 
